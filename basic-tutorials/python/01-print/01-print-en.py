823c376a3808c043cde4243a6ab8366f19fb2263
Marc Delgado Ferreres <marc.delgado.ferreres.mail@proton.me>
2024-08-09 05:12:28 +0200

    Create 01-print-en.py [marc-delgado-ferreres/basic-tutorials/python/01-print]

---
+++ basic-tutorials/python/01-print/01-print-en.py
@@ -0,0 +1,45 @@
+### Print ###
+
+# What is its function? #
+
+# The "print()" function is used to print or display messages, variable values, and any other content you want to show on the console.
+
+# Example 1 #
+
+print("Hello, world!")
+
+# In this example, the text "Hello, world!" is shown.
+# The "print()" function is used to display the text on the console.
+# As a result, it will print:
+# Hello, world!
+
+# Example 2 #
+
+print(7900000000)
+
+# In this example, the number 7900000000 is shown.
+# The "print()" function is used to display the number on the console.
+# As a result, it will print:
+# 7900000000
+
+# Example 3 #
+
+print("Hello, world!", 7900000000)
+
+# In this example, the text "Hello, world!" and the number 7900000000 are shown.
+# The "print()" function is used to display both on the console.
+# As a result, it will print:
+# Hello, world! 7900000000
+
+# Example 4 #
+
+print("\nEnter")
+
+# When using "\n", it creates a new line on the console.
+# In this example, the text "\nEnter" is shown.
+# The "print()" function is used to display the text on the console.
+# As a result, it will print:
+
+# Enter
+
+# Marc Delgado Ferreres
