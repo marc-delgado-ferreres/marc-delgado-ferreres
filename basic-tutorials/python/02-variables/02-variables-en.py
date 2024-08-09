cb34571cfe01093915f341a3d4136f15e3dc32a3
Marc Delgado Ferreres <marc.delgado.ferreres.mail@proton.me>
2024-08-09 05:13:27 +0200

    Create 02-variables-en.py [marc-delgado-ferreres/basic-tutorials/02-variables]

---
+++ basic-tutorials/python/02-variables/02-variables-en.py
@@ -0,0 +1,69 @@
+### Variables ###
+
+# What is their function? #
+
+# Variables are used to store and manipulate data by assigning a name to a value.
+
+# Types of Variables #
+
+# There are different types of variables.
+
+# Integer Variable [int] #
+
+# This variable can only be an integer number.
+
+integer = 10
+print(integer)
+print("The result is ", integer)
+print("The result is {}".format(integer))
+
+# In this example, a variable called "integer" is created and assigned the number 10.
+# Then, the "print()" function is used to display the content of the "integer" variable on the console.
+# As a result, it will print "10".
+# As a result, it will print "The result is 10".
+# As a result, it will print "The result is 10".
+
+# Floating-point Variable [float] #
+
+# This variable can only be a decimal number.
+
+floating = 9.81
+print(floating)
+print("The result is ", floating)
+print("The result is {}".format(floating))
+
+# In this example, a variable called "floating" is created and assigned the number 9.81.
+# Then, the "print()" function is used to display the content of the "floating" variable on the console.
+# As a result, it will print "9.81".
+# As a result, it will print "The result is 9.81".
+# As a result, it will print "The result is 9.81".
+
+# String Variable [str] #
+
+# This variable can only be a string of text.
+
+text_string = "Gravity"
+print(text_string)
+print("The result is ", text_string)
+print("The result is {}".format(text_string))
+
+# In this example, a variable called "text_string" is created and assigned the text "Gravity".
+# Then, the "print()" function is used to display the content of the "text_string" variable on the console.
+# As a result, it will print "Gravity".
+# As a result, it will print "The result is Gravity".
+# As a result, it will print "The result is Gravity".
+
+# Boolean Variable [bool] #
+
+# This variable can only be True or False.
+
+boolean = True
+print(boolean)
+print("The result is ", boolean)
+print("The result is {}".format(boolean))
+
+# In this example, a variable called "boolean" is created and assigned "True".
+# Then, the "print()" function is used to display the content of the "boolean" variable on the console.
+# As a result, it will print "True".
+
+# Marc Delgado Ferreres
