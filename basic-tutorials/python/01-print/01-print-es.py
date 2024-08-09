77e01b7a17579b2dba7415f4b6747071712252d5
Marc Delgado Ferreres <marc.delgado.ferreres.mail@proton.me>
2024-08-09 05:12:42 +0200

    Create 01-print-es.py [marc-delgado-ferreres/basic-tutorials/01-print]

---
+++ basic-tutorials/python/01-print/01-print-es.py
@@ -0,0 +1,45 @@
+### Print ###
+
+# Qué función tiene #
+
+# La función "print()"" se utiliza para imprimir o mostrar mensajes, valores de variables y cualquier otro contenido que se desee visualizar en la consola.
+
+# Ejemplo 1 #
+
+print("¡Hola, mundo!")
+
+# En este ejemplo, se muestra el texto "¡Hola, mundo!". 
+# Luego, se utiliza la función "print()" para mostrar el texto en la consola. 
+# Como resultado, se imprimirá: 
+# ¡Hola, mundo!
+
+# Ejemplo 2 #
+
+print(7900000000)
+
+# En este ejemplo, se muestra el número 7900000000. 
+# Luego, se utiliza la función "print()" para mostrar el número en la consola. 
+# Como resultado, se imprimirá: 
+# 7900000000
+
+# Ejemplo 3 #
+
+print("¡Hola, mundo!", 7900000000)
+
+# En este ejemplo, se muestra el texto "¡Hola, mundo!" y el número 7900000000. 
+# Luego, se utiliza la función "print()" para mostrar ambas cosas en la consola. 
+# Como resultado, se imprimirá: 
+# ¡Hola, mundo! 7900000000
+
+# Ejemplo 4 #
+
+print("\nEnter")
+
+# Cuando utilizamos "\n" haemos un enter en la consola.
+# En este ejemplo, podemos observar el texto "\Enter\n". 
+# Luego, se utiliza la función "print()" para mostrar el texto en la consola. 
+# Como resultado, se imprimirá:
+# 
+# Enter
+
+# Marc Delgado Ferreres
