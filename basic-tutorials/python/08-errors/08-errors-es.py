af7b25e9a62c1ff53ccc8ce28e81b4589ee4a582
Marc Delgado Ferreres <marc.delgado.ferreres.mail@proton.me>
2024-08-09 05:20:12 +0200

    Create 08-errors-en.py [marc-delgado-ferreres/basic-tutorials/python/08-errors]

---
+++ basic-tutorials/python/08-errors/08-errors-es.py
@@ -0,0 +1,47 @@
+### Errores ###
+
+# Qué función tiene #
+
+# Los errores tienen la función de indicar que ha ocurrido un problema durante la ejecución del programa. 
+# Cada tipo de error tiene un propósito específico y proporciona información sobre el tipo de error y su causa, lo que ayuda a identificar y corregir el problema.
+
+# Tipos de errores #
+
+# Existen diferentes tipos de errores.
+
+# Error: SyntaxError #
+
+# Causa: Falta un paréntesis de cierre
+
+print("Hola, mundo!"
+
+# Error: NameError #
+
+# Causa: La variable "x" no ha sido definida previamente
+
+suma = 100 + x
+
+# Error: TypeError #
+
+# Causa: No se puede sumar un entero con una cadena
+
+suma = 100 + "100"
+
+# Error: ValueError #
+
+# Causa: El valor "abc" no se puede convertir a un entero
+
+numero_entero = int("Hola, mundo!")
+
+# Error: IndexError #
+
+# Causa: Se intenta acceder a un elemento fuera del rango de la lista
+
+my_list = [1, 2, 3]
+element = my_list[3]
+
+# Error: FileNotFoundError #
+
+# Causa: El archivo "file.txt" no existe en el directorio actual
+
+# Marc Delgado Ferreres
