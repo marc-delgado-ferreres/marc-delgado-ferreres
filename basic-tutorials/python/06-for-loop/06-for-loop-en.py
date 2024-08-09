63c2c4af6185bd9b0ade29ee8f7cff98e8f2db5d
Marc Delgado Ferreres <marc.delgado.ferreres.mail@proton.me>
2024-08-09 05:18:18 +0200

    Create 06-for-loop-en.py [marc-delgado-ferreres/basic-tutorials/python/06-for-loop]

---
+++ basic-tutorials/python/06-for-loop/06-for-loop-en.py
@@ -0,0 +1,48 @@
+### For Loop ###
+
+# What is its purpose #
+
+# The "for" loop is used to iterate over a sequence of elements, such as a list, a string, or a range of numbers.
+# It allows you to execute a block of code repeatedly for each element in the sequence.
+
+# Example 1 #
+
+message = "Hello, world!"
+
+for character in message:
+    print(character)
+
+# In this example, the "for" loop is used to iterate over each character in the string "Hello, world!".
+# In each iteration, the character is printed to the console.
+# The result will be printing each character of the string on separate lines:
+# H
+# e
+# l
+# l
+# o
+# ,
+#  
+# w
+# o
+# r
+# l
+# d
+# !
+
+# Example 2 #
+
+# We can observe in this example that "range()" is used to generate a range between two numbers.
+
+for i in range(1, 6):
+    print(i)
+
+# In this example, the "for" loop is used to iterate over a range of numbers from 1 to 5.
+# In each iteration, the number is printed to the console.
+# The result will be printing the numbers from 1 to 5 on separate lines:
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# Marc Delgado Ferreres
