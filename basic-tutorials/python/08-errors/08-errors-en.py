f0ee7d1d58d81147add544ae540fd1cbbd985ee6
Marc Delgado Ferreres <marc.delgado.ferreres.mail@proton.me>
2024-08-09 05:20:12 +0200

    Create 08-errors-en.py [marc-delgado-ferreres/basic-tutorials/08-errors]

---
+++ basic-tutorials/python/08-errors/08-errors-en.py
@@ -0,0 +1,47 @@
+### Errors ###
+
+# What is its purpose #
+
+# Errors serve to indicate that there has been a problem during the execution of the program. 
+# Each type of error has a specific purpose and provides information about the type of error and its cause, which helps in identifying and fixing the issue.
+
+# Types of errors #
+
+# There are different types of errors.
+
+# Error: SyntaxError #
+
+# Cause: Missing closing parenthesis
+
+print("Hello, world!"
+
+# Error: NameError #
+
+# Cause: The variable "x" has not been defined previously
+
+sum = 100 + x
+
+# Error: TypeError #
+
+# Cause: Cannot add an integer to a string
+
+sum = 100 + "100"
+
+# Error: ValueError #
+
+# Cause: The value "abc" cannot be converted to an integer
+
+integer_number = int("Hello, world!")
+
+# Error: IndexError #
+
+# Cause: Attempting to access an element outside the range of the list
+
+my_list = [1, 2, 3]
+element = my_list[3]
+
+# Error: FileNotFoundError #
+
+# Cause: The file "file.txt" does not exist in the current directory
+
+# Marc Delgado Ferreres
