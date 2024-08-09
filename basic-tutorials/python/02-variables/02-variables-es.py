60e1eed9e5f54e0fa61e1f969e152aa3a99f8607
Marc Delgado Ferreres <marc.delgado.ferreres.mail@proton.me>
2024-08-09 05:13:27 +0200

    Create 02-variables-en.py [marc-delgado-ferreres/basic-tutorials/python/02-variables]

---
+++ basic-tutorials/python/02-variables/02-variables-es.py
@@ -0,0 +1,69 @@
+### Variables ###
+
+# Qué función tienen #
+
+# Las variables se utilizan para almacenar y manipular datos asignando un nombre a un valor.
+
+# Tipos de variables #
+
+# Existen diferentes tipos de variables.
+
+# Variable de tipo entero [int] #
+
+# Esta variable únicamente puede ser un número entero.
+
+entero = 10
+print(entero)
+print("El resultado es ", entero)
+print("El resultado es {}".format(entero))
+
+# En este ejemplo, se crea una variable llamada "entero" y se le asigna el número 10.
+# Luego, se utiliza la función "print()" para mostrar el contenido de la variable "entero" en la consola.
+# Como resultado, se imprimirá "10".
+# Como resultado, se imprimirá "El resultado es 10".
+# Como resultado, se imprimirá "El resultado es 10".
+
+# Variable de tipo flotante [float] #
+
+# Esta variable únicamente puede ser un número decimal.
+
+flotante = 9.81
+print(flotante)
+print("El resultado es ", flotante)
+print("El resultado es {}".format(flotante))
+
+# En este ejemplo, se crea una variable llamada "flotante" y se le asigna el número "9.81".
+# Luego, se utiliza la función "print()" para mostrar el contenido de la variable "flotante" en la consola.
+# Como resultado, se imprimirá "9.81".
+# Como resultado, se imprimirá "El resultado es 9.81".
+# Como resultado, se imprimirá "El resultado es 9.81".
+
+# Variable de tipo cadena de texto [str] #
+
+# Esta variable únicamente puede ser una cadenade texto.
+
+cadena_de_texto = "Gravedad"
+print(cadena_de_texto)
+print("El resultado es ", cadena_de_texto)
+print("El resultado es {}".format(cadena_de_texto))
+
+# En este ejemplo, se crea una variable llamada "cadena_de_texto" y se le asigna el texto "Gravedad".
+# Luego, se utiliza la función "print()" para mostrar el contenido de la variable "cadena_de_texto" en la consola.
+# Como resultado, se imprimirá "Gravedad".
+# Como resultado, se imprimirá "El resultado es Gravedad".
+# Como resultado, se imprimirá "El resultado es Gravedad".
+
+# Variable de tipo booleano [bool] #
+
+# Esta variable únicamente puede ser True o False.
+
+booleano = True
+print(booleano)
+print("El resultado es ", booleano)
+print("El resultado es {}".format(booleano))
+
+# En este ejemplo, se crea una variable llamada "booleano" y se le asigna "True".
+# Luego, se utiliza la función "print()" para mostrar el contenido de la variable "booleano" en la consola.
+# Como resultado, se imprimirá "True".
+
+# Marc Delgado Ferreres
